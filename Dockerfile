f84f1b2252dcb03c3640322ba968025a226a3940
Aditya Rachman Putra <adityarputra@gmail.com>
2022-03-01 14:28:48 +0700

    try EDA and dockerizing simple model

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM python:3.8
 
 WORKDIR /opt/app
 
-COPY . .
+COPY src/ ./src/
+COPY data/models/ ./data/models/
+COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
+# CMD ["ls","src"]
 CMD ["python", "/opt/app/src/app.py"]
--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import pickle
+from numpy import array2string
+
+model_path = "data/models/RandomForestClassifier_base.pkl"
+
+with open(model_path, "rb") as file:
+    unpickled_rf = pickle.load(file)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "hello world!"
+@app.route("/score", methods=["GET"])
+def predict_species():
+    features = ["petal_length", "petal_width","sepal_length","sepal_width"]
+    flower = [ request.args.get(feat) for feat in features]
+    return array2string(unpickled_rf.predict([flower]))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True, port=5001)
